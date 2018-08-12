40601b2047cf08ac43d48d684807b490a437a83d
iain-t-bennett <36244838+iain-t-bennett@users.noreply.github.com>
2018-08-12 20:16:02 +0200

    Update example3.py

--- example3.py
+++ example3.py
@@ -10,23 +10,27 @@ import time
 # as upside down in scrollbot
 sphd.rotate(180) 
 
-# check it works - cycle every pixel
+# check it works - light every pixel
 
 for x in range(17):
     sphd.clear()
     for y in range(7):
         sphd.set_pixel(x, y, 0.25)
-        time.sleep(0.05)        
-        sphd.show()
+sphd.show()
+time.sleep(0.1)        
+sphd.clear()
+sphd.show()
 
 # read string from website
 link = "https://raw.githubusercontent.com/iain-t-bennett/scrollbot/master/example3.msg"
 f = requests.get(link)
 
 msg = f.text
-sphd.write_string(msg)
 
-for x in range(len(msg)*10):
+sphd.write_string(msg)
+sphd.set_brightness(0.25)
+
+for x in range(len(msg)*3+17):
     sphd.show()
     sphd.scroll(1)
     time.sleep(0.05)
