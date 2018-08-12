c8f3c5efe1946c8d19bc44d42b703c33d48e3cbd
iain-t-bennett <36244838+iain-t-bennett@users.noreply.github.com>
2018-08-12 19:34:37 +0200

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,28 @@
+# test to scroll a message
+
+import scrollphathd as sphd
+import time
+
+# as upside down in scrollbot
+sphd.rotate(180) 
+
+# check it works - cycle every pixel
+
+for x in range(17):
+    sphd.clear()
+    for y in range(7):
+        sphd.set_pixel(x, y, 0.25)
+    sphd.show()
+    time.sleep(1/17.0)
+
+# write a string
+
+msg = 'This is a test'
+sphd.write_string(msg)
+
+for x in range(len(msg)):
+    sphd.show()
+    sphd.scroll(1)
+    time.sleep(0.05)
+
+# end of script
